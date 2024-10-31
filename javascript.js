dc418fc1820528a5f5513df5e0171164a5043367
tania <tanianurialifia23@gmail.com>
2024-10-31 15:01:04 +0700

    first

---
+++ javascript.js
@@ -0,0 +1,41 @@
+var tombolMenu = $(".tombol-menu");
+var menu = $("nav .menu ul");
+
+function klikMenu(){
+    tombolMenu.click(function(){
+        menu.toggle();
+    });
+    menu.click(function(){
+        menu.toggle();
+    });
+}
+
+$(document).ready(function () {
+    var width = $(window).width();
+    if (width < 990) {
+        klikMenu();
+    }
+})
+
+$(window).resize(function(){
+    var width = $(window).width();
+    if (width > 989) {
+        menu.css("display","block");
+    } else {
+        menu.css("display", "none");
+    }
+    klikMenu();
+});
+
+$(document).ready(function () {
+    var scroll_pos = 0;
+    $(document).scroll(function () {
+        // scroll_pos = $(this).scrollTop();
+        // if (scroll_pos > 0) {
+        //     $("nav").addClass("putih");
+           
+        // } else {
+        //     $("nav").removeClass("putih");
+        // }
+    })
+});
